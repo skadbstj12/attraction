3f1e38a8c9a46724350c4ead67dbd61711e0797b
skadbstj12 <163810643+skadbstj12@users.noreply.github.com>
2024-06-24 17:17:32 +0900

    2024/06/24

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,18 +15,20 @@ const Home = () => {
                     </div>
                     <div className='features'>
                         <div className='feature'>
+                            <img src="" alt="" />
                             <h2>최신 영화 예고편</h2>
-                            <p>최신 영화 예고편</p>
                             <button onClick={() => navigateToSearch('최신 영화 예고편')}>최신 영화 예고편</button>
                         </div>
                         <div className='feature'>
-                            <h2>최신 영화</h2>
+                                 <img src="" alt="" />
+                            <h2>최신 영화 리뷰</h2>
                             <p>최신 영화 리뷰를 찾아보세요.</p>
                             <button onClick={() => navigateToSearch('최신영화 리뷰')}>최신 영화 리뷰</button>
                         </div>
                         <div className='feature'>
+                            <img src="" alt="" />
                             <h2>인기 영화</h2>
-                            <p>평가가 좋은 영화들!!</p>
+                            <p>평점 높은 영화 리뷰</p>
                             <button onClick={() => navigateToSearch('인기 영화')}>인기 영화 리뷰</button>
                         </div>
                     </div>
